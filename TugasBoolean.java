f00fc7d233352230fe721e6b603b9e263455a9ad
unknown <Ibram Afriselly>
2014-10-14 21:51:15 +0700

    tugas boolean

---
+++ TugasBoolean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.pvt.latihanboolean;
+
+/**
+ *
+ * @author Personal
+ */
+public class TugasBoolean {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean hasil;
+        hasil = 2<5 && 6>2;
+        
+        System.out.println(hasil);
+    }
+    
+}
